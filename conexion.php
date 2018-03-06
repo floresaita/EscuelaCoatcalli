02bd154dddb0c37e9e2b8b5509225f3e42212e1a
Gaviota Antonia Herrera Prieto <35613309+GaviotaAntonia@users.noreply.github.com>
2018-03-05 23:23:39 -0600

    Cambios en conexion.php

--- conexion.php
+++ conexion.php
@@ -1,1 +1,13 @@
-conexion
+<?php
+function conectar();
+$user="root";
+$pass="12345678";
+$serv="localhost";
+$db="escuela_coatcalli";
+$con=mysql_connect($serv,$user,$pass) or die("error al conectar".mysql_error()));
+mysql_select_db($db,$con);
+
+
+return $con;
+}
+?>
